798c1295823507c9570ad499f03b333f4518f45d
Darmikon <hellfrom@mail.ru>
2018-05-11 23:07:14 +0300

    Update proxy

--- config/proxy.js
+++ config/proxy.js
@@ -2,7 +2,7 @@ module.exports = {
   // https://webpack.github.io/docs/webpack-dev-server.html#proxy
   // https://github.com/chimurai/http-proxy-middleware#options
   '/api': {
-    target: 'https://other-server.example.com',
+    target: process.env.API_URL || 'https://other-server.example.com',
     changeOrigin: true,
     secure: false,
     // headers: {
@@ -11,6 +11,6 @@ module.exports = {
     // },
     pathRewrite(path) {
       return path.replace(/^\/api/, ''); // remove '/api' prefix when requesting
-    }
-  }
+    },
+  },
 };
